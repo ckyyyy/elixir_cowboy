93076f39cb9a99d99bdd05a293faebf373b32a7f
ckyyyy <chongkyxxx@gmail.com>
2020-11-28 19:11:26 +0800

    git init

---
+++ lib/elixir_cowboy/application.ex
@@ -0,0 +1,20 @@
+defmodule ElixirCowboy.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: ElixirCowboy.Worker.start_link(arg)
+      # {ElixirCowboy.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ElixirCowboy.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
