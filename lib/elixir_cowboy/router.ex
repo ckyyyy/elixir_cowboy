0d29059b8a8a08b6bd91201d2adff0c5304ba364
ckyyyy <chongkyxxx@gmail.com>
2020-12-29 15:06:21 +0800

    refactor: catch all invalid routes using before_compile macro

--- lib/elixir_cowboy/router.ex
+++ lib/elixir_cowboy/router.ex
@@ -6,13 +6,4 @@ defmodule ElixirCowboy.Router do
 
   get("/", PageController, :home)
   get("/about", PageController, :about)
-
-  def content_for(conn, _) do
-    conn
-    |> Conn.put_resp_code(404)
-    |> Conn.put_resp_body("""
-    <h1>Page not found</h1>
-    <p>Error 404, page not found</p>
-    """)
-  end
 end
--- lib/toy/router.ex
+++ lib/toy/router.ex
@@ -2,10 +2,24 @@ defmodule Toy.Router do
   defmacro __using__(_opts) do
     quote do
       import Toy.Router
+      @before_compile Toy.Router
 
       def call(conn) do
         content_for(conn, conn.req_path)
       end
+    end
+  end
+
+  defmacro __before_compile__(_env) do
+    quote do
+      def content_for(conn, _) do
+        conn
+        |> Toy.Conn.put_resp_code(404)
+        |> Toy.Conn.put_resp_body("""
+          <h1>Page not found</h1>
+          <p>Error 404, page not found</p>
+          """)
+      end 
     end
   end
 
